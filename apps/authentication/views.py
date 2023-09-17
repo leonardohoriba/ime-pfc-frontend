b6a58dada8f90a3d2246dc3834f339344a63b784
Leonardo Horiba <leonardohoriba@gmail.com>
2023-09-17 19:14:56 -0300

    feature: login pages

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -7,6 +7,7 @@ Copyright (c) 2019 - present AppSeed.us
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm, SignUpForm
+from django.contrib.auth.decorators import user_passes_test
 
 
 def login_view(request):
@@ -31,6 +32,11 @@ def login_view(request):
     return render(request, "accounts/login.html", {"form": form, "msg": msg})
 
 
+# Função que verifica se o usuário é um administrador
+def is_admin(user):
+    return user.username == 'admin' if user.is_authenticated else False
+
+@user_passes_test(is_admin, login_url='/login/.html')
 def register_user(request):
     msg = None
     success = False
@@ -43,10 +49,10 @@ def register_user(request):
             raw_password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=raw_password)
 
-            msg = 'User created successfully.'
+            msg = 'Usuário criado com sucesso.'
             success = True
 
-            # return redirect("/login/")
+            return redirect("/")
 
         else:
             msg = 'Form is not valid'
